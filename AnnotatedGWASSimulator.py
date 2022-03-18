8f6b525572aaaa5a151535b1f893dee2d2c4baf7
Shadi <shadizabad1@gmail.com>
2022-03-18 16:40:26 -0400

    Incorporating enrichments into simulation of annotation weights for per-SNP causal probability

--- AnnotatedGWASSimulator.py
+++ AnnotatedGWASSimulator.py
@@ -11,50 +11,64 @@ class AnnotatedGWASSimulator(GWASSimulator):
         # For now, we will restrict to 2 mixture components.
         assert self.n_mixtures == 2
 
-        self.w_theta = None
-        self.w_phi = None
+        self.w_h2 = None
+        self.w_pi = None
 
-    def set_w_theta(self, w_theta):
+    def set_w_h2(self, w_h2):
 
-        assert len(w_theta) == self.n_annotations
+        assert len(w_h2) == self.n_annotations
 
-        self.w_theta = w_theta
+        self.w_h2 = w_h2
         self.set_per_snp_heritability()
 
-    def simulate_w_theta(self, enrichment=None):
+    def simulate_w_h2(self, enrichment=None):
         pass
 
-    def set_w_phi(self, w_phi):
+    def set_w_pi(self, w_pi):
 
-        assert len(w_phi) == self.n_annotations
+        assert len(w_pi) == self.n_annotations
 
-        self.w_phi = w_phi
+        self.w_pi = w_pi
         self.set_per_snp_mixture_probability()
 
-    def simulate_w_phi(self, enrichment=None):
-        pass
+    def simulate_w_pi(self, enrichment=None):
+        """
+        :param enrichment: A dictionary of enrichment values where the
+        key is the annotation and the value is the enrichment
+        """
+
+        enrichment = enrichment or {}
+        enr = []
+        for annot in self.annotations[self.chromosomes[0]].annotations:
+            try:
+                enr.append(enrichment[annot])
+            except KeyError:
+                enr.append(1.)
+
+        self.w_pi = np.log(np.array(enr))
 
     def set_per_snp_heritability(self):
 
-        if self.w_theta is None:
+        if self.w_h2 is None:
             return super().set_per_snp_heritability()
 
         self.per_snp_h2g = {}
 
         for c in self.chromosomes:
-            self.per_snp_h2g[c] = np.clip(np.dot(self.annotations[c].values(), self.w_theta), a_min=0., a_max=np.inf)
+            self.per_snp_h2g[c] = np.clip(np.dot(self.annotations[c].values(), self.w_h2),
+                                          a_min=0., a_max=np.inf)
 
     def set_per_snp_mixture_probability(self):
 
-        if self.w_phi is None:
+        if self.w_pi is None:
             return super().set_per_snp_mixture_probability()
 
         self.per_snp_pi = {}
 
         for c in self.chromosomes:
-            prob = 1./(1. + np.exp(-np.dot(self.annotations[c].values(), self.w_phi)))
+            prob = 1./(1. + np.exp(-np.dot(self.annotations[c].values(add_intercept=True),
+                                           np.concatenate([[np.log(self.pi[1])], self.w_pi]))))
             self.per_snp_pi[c] = np.array([1. - prob, prob]).T
-            print(self.per_snp_pi[c])
 
     def get_heritability_enrichment(self):
 
